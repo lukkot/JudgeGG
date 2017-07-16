4895629a2b7844893ecc7ab3bab2e957a2ea77ed
Lukasz Kotynski <lkotynski@rambus.com>
2017-07-16 22:17:00 +0200

    Poszerzenie grona autorów ;)

--- src/main/java/pl/gymkhana_gp/judge/presentation/views/AboutInfoController.java
+++ src/main/java/pl/gymkhana_gp/judge/presentation/views/AboutInfoController.java
@@ -11,7 +11,7 @@ public class AboutInfoController {
 		Alert alert = new Alert(AlertType.INFORMATION);
 		alert.setTitle("O programie");
 		alert.setHeaderText("O programie.");
-		alert.setContentText("Autor:\n" + " - Łukasz Kotyński\n" + "Dla:\n"
+		alert.setContentText("Autorzy:\n" + " - Łukasz Kotyński\n" + " - Filip Pająk\n" + "Dla:\n"
 				+ " - Klub Motocyklistów Politechniki Łódzkiej\n" + " - Gymkhana GP\n" + "Informacje o grafikach:\n"
 				+ " - Designed by Dave Gandy, Freepik, Gregor Cresnar from Flaticon.\n");
 
